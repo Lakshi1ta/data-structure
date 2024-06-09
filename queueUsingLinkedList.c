71d6f06d9ab671df97f2379181277418048963cf
Lakshita Kandpal <149196032+Lakshi1ta@users.noreply.github.com>
2024-06-09 16:05:35 +0530

    Add files via upload

---
+++ queueUsingLinkedList.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+}*front=0,*rear=0,*newnode,*temp;
+int main() {
+    enque();
+    enque();
+    enque();
+    deque();
+    display();
+}
+void enque() {
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("enter data\n");
+    scanf("%d",&newnode->data);
+    newnode->next=NULL;
+    if(front==NULL && rear==NULL){
+        front=rear=newnode;
+    }
+    else{
+        rear->next=newnode;
+        rear=newnode;
+    }
+}
+void display(){
+    temp=front;
+    if(front==NULL && rear==NULL){
+        printf("empty queue\n");
+    }
+    else{
+        while(temp!=NULL){
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }
+    }
+} void deque(){
+    printf(" queue is");
+    if(front==NULL&& rear==NULL){
+        printf("empty queue\n");
+    }
+    else{
+        temp=front;
+        front=front->next;
+        free(temp);
+    }
+}
